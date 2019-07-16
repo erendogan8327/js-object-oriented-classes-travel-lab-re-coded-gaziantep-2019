97e259b3599aafb441cc792b2fe3fdccbc43482d
Eren Dogan <eren.dogan@gaziulas.com.tr>
2019-07-16 19:39:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
-class Driver {
-  constructor(name, startDate) {
-    this.name = name;
-    this.startDate = new Date(startDate);
+class Driver{
+  constructor(name, startDate){
+    this.name=name;
+    this.startDate=new Date(startDate);
   }
-
-  yearsExperienceFromBeginningOf(year) {
-    return (year - this.startDate.getFullYear() -1)
+  
+ 
+  yearsExperienceFromBeginningOf(day2){
+    let date1 = new Date(this.startDate);
+    let date2 = new Date(day2);
+    return date2.getFullYear()-date1.getFullYear();
   }
 }
-
-const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
-
-class Route {
-  constructor(start, end) {
-  this.start = start
-  this.end = end
+class Route{
+  constructor(beginningLocation,endingLocation){
+    this.beginningLocation=beginningLocation;
+    this.endingLocation=endingLocation;
   }
+  
 
   blocksTravelled(){
-    const verticalDistance = (this.end.vertical - this.start.vertical)
-    const horizontalDistance = (eastWest.indexOf(this.end.horizontal) - eastWest.indexOf(this.start.horizontal))
-    return verticalDistance + horizontalDistance;
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
+      let fark= Math.abs(eastWest.indexOf(this.beginningLocation.horizontal)-eastWest.indexOf(this.endingLocation.horizontal))+Math.abs(this.beginningLocation.vertical-this.endingLocation.vertical)
+      return fark;
   }
+  estimatedTime(peak){
+  let zaman1= this.blocksTravelled()/3;
+  let zaman2= this.blocksTravelled()/2;
+     if(peak)return zaman2 ;
+     else return zaman1;
+   
+}
 
-  estimatedTime(peak = false){
-      if(peak === true){
-        return this.blocksTravelled() / 2
-      }else{
-        return this.blocksTravelled() / 3
-      }
-  }
 }
